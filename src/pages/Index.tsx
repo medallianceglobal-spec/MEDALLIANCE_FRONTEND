6d2245f36a11308c730a2c7bbd1b25994ad262fb
medallianceglobal-spec <medallianceglobal@gmail.com>
2025-12-22 20:29:08 +0530

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -281,8 +281,8 @@ const Index = () => {
       </section>
 
       {/* HOW MEDALLIANCE WORKS */}
-      <section className="py-20 bg-muted/30 -mt-4">
-        <div className="container mx-auto px-4">
+      <section className="py-20 bg-muted/30 ">
+        <div className="container mx-auto px-4 -mt-10">
           <div className="text-center mb-14">
             <h2 className="text-3xl md:text-4xl font-bold">
               How MedAlliance Works

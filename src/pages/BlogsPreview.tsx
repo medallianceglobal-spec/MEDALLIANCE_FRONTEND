426b5589298292a8ce0c6e305194b49e51b370ef
medallianceglobal-spec <medallianceglobal@gmail.com>
2025-12-22 20:27:02 +0530

    Update BlogsPreview.tsx

--- src/pages/BlogsPreview.tsx
+++ src/pages/BlogsPreview.tsx
@@ -224,7 +224,7 @@ const BlogsPreview = () => {
         </section>
 
         {/* Our Blogs Heading */}
-        <section className="pb-8">
+        <section className="pb-8 mt-10">
           <div className="container mx-auto px-4 text-center">
             <h3 className="text-2xl md:text-3xl font-bold">
               Our Blogs

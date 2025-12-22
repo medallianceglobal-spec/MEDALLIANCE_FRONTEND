f22317d4a4f1d1bf61cfcad969b044a98472985a
medallianceglobal-spec <medallianceglobal@gmail.com>
2025-12-22 14:43:58 +0530

    Update Testimonials.tsx

--- src/pages/Testimonials.tsx
+++ src/pages/Testimonials.tsx
@@ -182,7 +182,7 @@ const TestimonialsSection = () => {
         }
       `}</style>
       
-      <section className="py-24 bg-navy/70 overflow-hidden">
+      <section className="py-24 bg-blue-950 overflow-hidden">
         <div className="relative">
           <div className="flex items-center justify-center gap-6 px-4">
 
